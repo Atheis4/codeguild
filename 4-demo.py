261c203b3b282bb455dbbf4fbc8cd84d3f75a3f7
Andrew <andrewtheis4$gmail.com>
2016-03-18 13:22:38 -0700

    committing 4-demo.py to repository

---
+++ 4-demo.py
@@ -0,0 +1,21 @@
+class_name_to_roster = {}
+
+accept_input = True
+while accept_input:
+
+    print('Student name?')
+    student_name = input('> ')
+    accept_input = student_name != 'done'
+
+    if accept_input:
+        print('What class is ' + student_name + ' in?')
+        class_name = input('> ')
+
+        if class_name in class_name_to_roster:
+            old_roster = class_name_to_roster[class_name]
+        else:
+            old_roster = set()
+        new_roster = old_roster | {student_name}
+        class_name_to_roster[class_name] = new_roster
+
+print(class_name_to_roster)
