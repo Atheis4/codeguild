43607ae8e0fa67341026dbdc502ecb84205cccc3
Andrew <andrewtheis4$gmail.com>
2016-03-18 18:28:07 -0700

    first commit of pig_latin.py: need to check for issues and refine code.

--- pig_latin.py
+++ pig_latin.py
@@ -3,18 +3,21 @@ sentence = input('Veni, vidi, vici! What would you like to translate?\n> ')
 to_be_translated = sentence.split()
 
 pig_sentence = []
-vowels = 'aeiou'
+vowels = 'aeiouAEIOU'
 punctuation = '.!,;:'
 
 for word in to_be_translated:
 
-    if word[0] in vowels:
+    word = word.lower()
+
+    if word[0] in vowels and word[-1] in punctuation:
+        piggy = ''.join(word[:-1] + 'yay' + word[-1])
+    elif word[0] in vowels:
         piggy = ''.join(word + 'yay')
     elif word[-1] in punctuation:
         piggy = ''.join(word[1:-1] + word[0] + 'ay' + word[-1])
     else:
-        oink = ''.join(word[:1].lower() + 'ay')
-        piggy = ''.join(word[1:] + oink)
+        piggy = ''.join(word[1:] + ''.join(word[:1].lower() + 'ay'))
 
     pig_sentence.append(piggy)
 
