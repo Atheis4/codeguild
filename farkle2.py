fc239e714b4e88e68a6c6c6628cfd4f4bea64970
Andrew <andrewtheis4$gmail.com>
2016-03-26 20:37:02 -0700

    neglected commit

--- farkle2.py
+++ farkle2.py
@@ -1,11 +1,9 @@
-import random
-import sys
+import random, sys
 
 total_dice = 5
 rolls_list = []
 
 def roll_dice_add_to_list(total_dice, rolls_list):
-    """Generates a random number and appends it onto a list of our dice rolls"""
     while total_dice > 0:
         dice_roll = random.randint(1, 6)
         rolls_list.append(dice_roll)
@@ -13,7 +11,6 @@ def roll_dice_add_to_list(total_dice, rolls_list):
     return rolls_list
 
 def generate_dict_from_rolls_list(rolls_list):
-    """Generates a dictionary of values from our list of dice rolls, clears the dictionary for multi-use"""
     roll_history_dict = {}
     roll_history_dict.clear()
 
@@ -22,7 +19,6 @@ def generate_dict_from_rolls_list(rolls_list):
     return roll_history_dict
 
 def calculate_score(roll_history_dict):
-    """Runs the dictionary of roll values through our scoring system"""
     points = 0
 
     for k, v in roll_history_dict.items():
@@ -42,57 +38,68 @@ def calculate_score(roll_history_dict):
     return points
 
 def check_for_farkle(points):
-    """Checks point total for Farkle and quits program"""
     if points == 0:
         print('Farkle')
         sys.exit()
 
-def reroll_bool(points):
-    """Asks user if they want to reroll dice, if no, print score and quit program"""
-    reroll_prompt = input('Would you like to reroll any dice? (Y/N)\n> ')
-    if reroll_prompt == 'n' or reroll_prompt == 'N':
+def prompt_user_for_reroll(keep_rolling, points):
+    re_roll = input('Would you like to reroll any dice? (Y/N)\n> ')
+
+    if 'Nn' in re_roll:
         print('You have {} points. Thank\'s for playing.'.format(points))
-        sys.exit()
+        keep_rolling = False
+    return keep_rolling
 
 def return_dice_for_reroll(rolls_list):
-    """Asks for dice to be returned until ValueError & Removes returned dice from list of rolls"""
-    while True:
-        try:
-            # When using, try to make 'try' blocks as short as possible.
-            print(rolls_list)
-            remove_dice = int(input('Which dice would you like to reroll.\nType the dice number or \'done\'\n> '))
-            rolls_list.remove(remove_dice)
-        except ValueError:
-            return rolls_list
+    remove_dice = 0
 
-            # Don't use exceptions for control flow -- especially across function boundaries.
-            # input line - check to see if input is done -- if not done, then convert to int.
+    while remove_dice != 'done':
+        print(rolls_list)
+        remove_dice = input('Which dice would you like to reroll?\nType the dice number or \'done\' to continue.\n> ')
+        remove_dice = int(remove_dice)
+        rolls_list.remove(remove_dice)
+        if remove_dice != int():
+            print(rolls_list)
+
+    print(rolls_list)
+    return rolls_list
+
+
+
+# def reroll_bool(points):
+#     reroll_prompt = input('Would you like to reroll any dice? (Y/N)\n> ')
+#     if reroll_prompt == 'n' or reroll_prompt == 'N':
+#         print('You have {} points. Thank\'s for playing.'.format(points))
+#         sys.exit()
+#
+# def return_dice_for_reroll(rolls_list):
+#     while True:
+#         try:
+#             # When using, try to make 'try' blocks as short as possible.
+#             print(rolls_list)
+#             remove_dice = int(input('Which dice would you like to reroll.\nType the dice number or \'done\'\n> '))
+#             rolls_list.remove(remove_dice)
+#         except ValueError:
+#             return rolls_list
+#
+#             # Don't use exceptions for control flow -- especially across function boundaries.
+#             # input line - check to see if input is done -- if not done, then convert to int.
 
 def add_reroll_to_total_dice(rolls_list):
-    """Checks the length of our roll list subtracted by five (total dice) to determine total dice to be rerolled"""
     total_dice = 5 - len(rolls_list)
     return total_dice
 
 def print_score(rolls_list, points):
-    """Print point total and current dice list to user"""
     print('You have {} points'.format(points))
     print('Your current dice:', rolls_list)
 
-rolls_list = roll_dice_add_to_list(total_dice, rolls_list)
-roll_history_dict = generate_dict_from_rolls_list(rolls_list)
-points = calculate_score(roll_history_dict)
 
-print_score(rolls_list, points)
-
-check_for_farkle(points)
-reroll_bool(points)
-return_dice_for_reroll(rolls_list)
-total_dice = add_reroll_to_total_dice(rolls_list)
-
-rolls_list = roll_dice_add_to_list(total_dice, rolls_list)
-roll_history_dict = generate_dict_from_rolls_list(rolls_list)
-points = calculate_score(roll_history_dict)
-
-print_score(rolls_list, points)
-
-check_for_farkle(points)
+keep_rolling = True
+while keep_rolling:
+    rolls_list = roll_dice_add_to_list(total_dice, rolls_list)
+    roll_history_dict = generate_dict_from_rolls_list(rolls_list)
+    points = calculate_score(roll_history_dict)
+    print_score(rolls_list, points)
+    check_for_farkle(points)
+    prompt_user_for_reroll(keep_rolling, points)
+    rolls_list = return_dice_for_reroll(rolls_list)
