e9aa4ef0be51e3463d1ff0c3ca39f64f55556342
Andrew <andrewtheis4$gmail.com>
2016-03-29 16:29:33 -0700

    nth commit: changed sumthin... i dunno.

--- number_guess.py
+++ number_guess.py
@@ -1,21 +1,31 @@
 import random
 
-secret_number = random.randint(1, 100)
+def get_secret_number():
+    secret_number = random.randint(1, 100)
+    return secret_number
 
-guess = 0
+def prompt_user():
+    user_guess = input('Guess a number between 1 and 100:\n> ')
+    return user_guess
 
-print('Guess a number between 1 and 100:')
+def check_guess_to_secret_number(secret_number, user_guess):
+    while user_guess != secret_number:
+        compare_guess_to_secret(secret_number, user_guess)
+        prompt_user()
+        
+    if user_guess == secret_number:
+        print('You win!')
 
-while guess != secret_number:
-    guess = int(input('> '))
-
-    if guess < secret_number:
+def compare_guess_to_secret(secret_number, user_guess):
+    if user_guess < secret_number:
         print('Too low')
-    elif guess > secret_number:
+    elif user_guess > secret_number:
         print('Too high')
 
-print('You got it!')
 
+secret_number = get_secret_number()
+user_guess = prompt_user()
+check_guess_to_secret_number(secret_number, user_guess)
 
 
 
