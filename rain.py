82b1ebd5bb8b0da27083bd0721b181222d9042be
Andrew <andrewtheis4$gmail.com>
2016-04-03 20:38:05 -0700

    added option for user to input date and get predicted weather, looked at over all data structure and I definitely think there is improvement to be made.

--- rain.py
+++ rain.py
@@ -6,38 +6,28 @@ def import_web_data():
         lines = [byte_line.decode('utf-8') for byte_line in website]
     return lines
 
-
-
 def strip_split_slice_web_data(lines):
     new_line_list = [line.strip()[:17].split() for line in lines[11:]]
     return new_line_list
 
-
-
 def get_rid_of_null_data_turn_numbers_to_int(new_line_list):
-    day_month_year_list, y = list(zip(*new_line_list))
+    """raw y data in hundreths of inches. Conversion happens in print functions"""
+    x, y = list(zip(*new_line_list))
     y = ' '.join(y).replace('-', '0').split()
     rainfall_int_list = []
     for item in y:
-        rainfall_int_list.append(int(item))
-
-    data_list_tuple = list(zip(day_month_year_list, rainfall_int_list))
-    return data_list_tuple
-
-
-
-def create_dict_from_data_tuple(data_list_tuple):
-    rainfall_dict = {x: y for x, y in data_list_tuple}
+        rainfall_int_list.append(float(item)*.01)
+    date_to_rain_tuple = list(zip(x, rainfall_int_list))
+    return date_to_rain_tuple
+#
+def create_dict_from_data_tuple(date_to_rain_tuple):
+    rainfall_dict = {x: y for x, y in date_to_rain_tuple}
     return rainfall_dict
-
-
 
 def print_single_day_with_most_rain(rainfall_dict):
     rainfall_to_date_tuple_ascending = sorted([(v, k) for (k, v) in rainfall_dict.items()], reverse=True)
     v, k = zip(*rainfall_to_date_tuple_ascending)
-    print('The date with the most rainfall was', k[0], '\nwith', (v[0] * .01), 'inches of rain.')
-
-
+    print('The date with the most rainfall was {0} with\n{1:.2f} inches of rain.'.format(k[0], v[0]))
 
 def create_year_to_rain_total_dict(rainfall_dict):
     year_to_rainfall_total_dict = {}
@@ -46,65 +36,56 @@ def create_year_to_rain_total_dict(rainfall_dict):
 
     for x, y in rainfall_dict.items():
         year_to_rainfall_total_dict[x[-4:]] += y
+
     return year_to_rainfall_total_dict
-
-
 
 def print_year_with_most_rain(year_to_rainfall_total_dict):
     total_rain_to_year_tuple_ascending = sorted([(v, k) for (k, v) in year_to_rainfall_total_dict.items()], reverse=True)
     v, k = zip(*total_rain_to_year_tuple_ascending)
-    print('The year with the most rain was', k[0], '\nwith', (v[0] * .01), 'inches of rain.')
-
-
+    print('The year with the most rain was {0} with\n{1:.2f} inches of rain.'.format(k[0], v[0]))
 
 def create_avg_day_to_rain_total_dict(rainfall_dict):
     day_to_rainfall_total_dict = {}
-    number_of_each_date_in_data = {}
     for date in rainfall_dict.keys():
-        day_to_rainfall_total_dict[date[:-5]] = 0
-        number_of_each_date_in_data[date[:-5]] = 0
+        day_to_rainfall_total_dict[date[:-5]] = []
 
     for x, y in rainfall_dict.items():
-        day_to_rainfall_total_dict[x[:-5]] += y
-        number_of_each_date_in_data[x[:-5]] += 1
-
-    for x, y in number_of_each_date_in_data.items():
-        if x in day_to_rainfall_total_dict.keys():
-            day_to_rainfall_total_dict[x] /= y
+        day_to_rainfall_total_dict[x[:-5]].append(y)
 
     return day_to_rainfall_total_dict
 
-
-
 def print_date_with_most_rain_on_average(day_to_rainfall_total_dict):
-    total_rain_to_day_tuple_ascending = sorted([(v, k) for (k, v) in day_to_rainfall_total_dict.items()], reverse=True)
-    v, k = zip(*total_rain_to_day_tuple_ascending)
-    print('The date with the most rain on average is the', k[0], '\nwith', (v[0] * .01), 'inches of rain.')
+    avg_rainfall_list = []
+    for a, b in day_to_rainfall_total_dict.items():
+        avg_rainfall_list.append(statistics.mean(day_to_rainfall_total_dict[a]))
+    most_rainfall = max(avg_rainfall_list)
+    value_key_tuple = [(statistics.mean(v), k) for (k, v) in day_to_rainfall_total_dict.items()]
+
+    for v, k in value_key_tuple:
+        if v == most_rainfall:
+            print('The day with the most rain on average is {0} with\n{1:.2f} inches of rain.'.format(k, v))
 
 
 lines = import_web_data()
 new_line_list = strip_split_slice_web_data(lines)
-
-data_list_tuple = get_rid_of_null_data_turn_numbers_to_int(new_line_list)
-
-rainfall_dict = create_dict_from_data_tuple(data_list_tuple)
+date_to_rain_tuple = get_rid_of_null_data_turn_numbers_to_int(new_line_list)
+rainfall_dict = create_dict_from_data_tuple(date_to_rain_tuple)
+print()
 print_single_day_with_most_rain(rainfall_dict)
 year_to_rainfall_total_dict = create_year_to_rain_total_dict(rainfall_dict)
+print()
 print_year_with_most_rain(year_to_rainfall_total_dict)
 day_to_rainfall_total_dict = create_avg_day_to_rain_total_dict(rainfall_dict)
+print()
 print_date_with_most_rain_on_average(day_to_rainfall_total_dict)
 
 
+user_choice = True
 
-
-
-# USER INPUT - March 24th
-#
-# daily_rainfall_per_year = []
-#
-# [daily_rainfall_per_year.append(v) for k, v in rainfall_dict.items() if k.endswith('02')]
-#
-#
-#
-#
-# mean_rainfall_by_year = total_rainfall_by_year / len(daily_rainfall_per_year)
+while user_choice:
+    print()
+    date_lookup = input('Would you like to know the future\'s weather?\n(Enter date as DD-MMM, ex. 04-MAR - type \'done\' to quit\n> ')
+    if date_lookup in day_to_rainfall_total_dict.keys():
+        print('(If past is prologue: On', date_lookup, 'it might rain', statistics.mean(day_to_rainfall_total_dict[date_lookup]), 'inches.\nOr not.')
+    if date_lookup == 'done':
+        user_choice = False
