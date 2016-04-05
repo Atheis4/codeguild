d5fa2ac709b121aa4c65a033399f54b84e4786a3
Andrew <andrewtheis4$gmail.com>
2016-04-04 22:57:16 -0700

    commits for city_loop.py--first--and gutenberg2.py--nth--added user word choice and most-likely pairs.

--- city_loop.py
+++ city_loop.py
@@ -20,22 +20,21 @@ def append_to_visited_cities(current_city):
     return visited_cities
 
 def generate_next_city_list(d, current_city, visited_cities):
-    next_stop = []
-    print('You are in', current_city)
+    next_stops = []
     for city in d[current_city]:
         if city not in visited_cities:
-            next_stop.append(city)
-    return next_stop
+            next_stops.append(city)
+    return next_stops
 
-def prompt_user_for_next_city(next_stop, visited_cities):
+def prompt_user_for_next_city(next_stops, visited_cities):
     city_choice = True
     while city_choice:
-        print('Available destinations', next_stop)
+        print(visited_cities)
+        print('Available destinations', next_stops)
         current_city = input('Which city shall we travel to next?\n> ')
         if current_city in next_stop and current_city not in visited_cities:
             return current_city
             city_choice = False
-
 
 
 
@@ -51,4 +50,46 @@ visited_cities = append_to_visited_cities(current_city)
 next_stop = generate_next_city_list(d, current_city, visited_cities)
 current_city = prompt_user_for_next_city(next_stop, visited_cities)
 
-print(visited_cities)
+
+
+
+
+# # Practice: Road Trip
+# In a faraway land, nearby cities are connected by roads.
+# We've mapped what cities are directly connected by roads and stored them in a dict:
+# ```python
+# city_to_accessible_cities = {
+#   'Boston': {'New York', 'Albany', 'Portland'},
+#   'New York': {'Boston', 'Albany', 'Philadelphia'},
+#   'Albany': {'Boston', 'New York', 'Portland'},
+#   'Portland': {'Boston', 'Albany'},
+#   'Philadelphia': {'New York'}
+# }
+# ```
+# Traveling from one city to an adjacent one is called a **hop**.
+# ​
+# For this sort of problem, you'll want to iteratively visit cities you know you can access while updating:
+# 1. Cities you can access.
+# 1. Cities you've been to.
+# 1. Cities you haven't been to.
+# ​
+# * Let the user enter a city.
+# Print out all the cities connected to that city.
+# * Let the user enter a city.
+# Print out all cities that could be reached through two hops.
+# ​
+# ## Advanced
+# * Let the user enter a city and a number of hops.
+# Print out all cities that could be reached through that number of hops.
+# * We've also mapped the travel time of each hop:
+# ```python
+# city_to_accessible_cities_with_travel_time = {
+#   'Boston': {('New York', 4), ('Albany', 6), ('Portland', 3)},
+#   'New York': {('Boston', 4), ('Albany', 5), ('Philadelphia', 9)},
+#   'Albany': {('Boston', 6), ('New York',5), ('Portland', 7)},
+#   'Portland': {('Boston', 3), ('Albany', 7)},
+#   'Philadelphia': {('New York', 9)}
+# }
+# ```
+# When the user enters a city and a number of hops, print out the shortest travel times to each city.
+# Paths with fewer hops are not necessarily those with the shorter travel times.
--- gutenberg2.py
+++ gutenberg2.py
@@ -1,3 +1,5 @@
+import statistics
+
 def open_file_text():
     """opens text file and saves as readlines"""
     with open('alice_in_wonderland.txt') as text_file:
@@ -47,11 +49,11 @@ def create_word_pair_count(paired_word_dict, word_pair_list):
     return word_pair_count
 
 def zip_count_to_word_pair_tuple(word_pair_list, word_pair_count):
-    new_triplet = set(zip(word_pair_count, word_pair_list))
-    return new_triplet
+    count_to_word_pair_tuple = set(zip(word_pair_count, word_pair_list))
+    return count_to_word_pair_tuple
 
-def sort_triplet_find_top_pairs(new_triplet):
-    count_to_pairs_ascending_order = sorted(new_triplet, reverse=True)
+def sort_triplet_find_top_pairs(count_to_word_pair_tuple):
+    count_to_pairs_ascending_order = sorted(count_to_word_pair_tuple, reverse=True)
     for k, v in count_to_pairs_ascending_order[:10]:
         print('The word pair {} appears {} times in this text.'.format(v, k))
 
@@ -59,13 +61,43 @@ def sort_triplet_find_top_pairs(new_triplet):
 text_file = open_file_text()
 word_list_stripped = strip_text_to_list(text_file)
 single_word_dict = create_dict_from_word_list_stripped(word_list_stripped)
-find_top_ten_words(single_word_dict)
+# find_top_ten_words(single_word_dict)
 
 word_pair_list = zip_word_list_pairs(word_list_stripped)
 paired_word_dict = create_and_populate_word_pair_dict(word_pair_list)
 word_pair_count = create_word_pair_count(paired_word_dict, word_pair_list)
-new_triplet = zip_count_to_word_pair_tuple(word_pair_list, word_pair_count)
+count_to_word_pair_tuple = zip_count_to_word_pair_tuple(word_pair_list, word_pair_count)
 
-count_to_pairs_ascending_order = sort_triplet_find_top_pairs(new_triplet)
+# count_to_pairs_ascending_order = sort_triplet_find_top_pairs(count_to_word_pair_tuple)
+
+
+
+
+count_word_pair_triplet = zip(word_pair_count, word_list_stripped, word_list_stripped[1:])
+
 
 # ADVANCED -----------------
+
+
+paired_word_dict = {
+    key: {
+        k: v / sum(paired_word_dict[key].values()) for k, v in paired_word_dict[key].items()
+        }
+        for key in paired_word_dict.keys()
+    }
+
+user_lookup = True
+
+while user_lookup:
+    user_word = input('Type in a word and I\'ll see if it is in our text. If it is\nI\'ll give you the next most likely word to follow.\n(Type \'done\' to quit)\n> ')
+    if user_word not in paired_word_dict.keys():
+        print('That word isn\'t in our book')
+    if user_word == 'done':
+        user_lookup = False
+    if user_word in paired_word_dict.keys():
+        most_probable_next_word = max(paired_word_dict[user_word].values())
+        for x, y in paired_word_dict[user_word].items():
+            if y == most_probable_next_word:
+                print('The next most probable word is \'' + x + '\'')
+                print('Possibilities:', paired_word_dict[user_word].keys())
+                print()
