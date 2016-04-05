f6035b846068dca881f24ff6a4fd1f31546fa24d
Andrew <andrewtheis4$gmail.com>
2016-04-05 11:53:03 -0700

    2nd commit of review problem

--- busi_review.py
+++ busi_review.py
@@ -20,69 +20,75 @@ raw_business_data = [
   },
 ]
 
+
 class Review:
     """Business Review"""
     def __init__(self, rating, text):
         self.rating = rating
         self.text = text
 
-    def __str__(self):
-        return 'Review: rating {}, text \'{}\''.format(
+    def __repr__(self):
+        return 'Review(rating: {}, text: \'{}\')'.format(
             self.rating, self.text
         )
-    # def rating(self):
 
 
 class Business:
     """Business"""
-    def __init__(self, name):
+    def __init__(self, name, reviews):
         self.name = name
+        self.reviews = reviews
 
-    def __str__(self):
-        return 'Business name: {}'.format(
-            self.name
+    def __repr__(self):
+        return 'Business(name: {}, reviews: {})'.format(
+            self.name, self.reviews
         )
 
     def average_rating(self, rating):
-        self.rating = statistics.mean(self.rating)
+        pass
 
 
 
 list_of_reviews = []
 
 for dicts in raw_business_data:
+    # print(dicts)
+    Business(dicts['business_name'], dicts['reviews'])
     for review in dicts['reviews']:
         x = review['rating']
         y = review['text']
         list_of_reviews.append(Review(x, y))
 
-print(Review)
 
 
 
-
-
-# print(raw_business_data[0]['reviews'][0]['rating'])
-# print(raw_business_data[0]['reviews'][0]['text'])
+# for i in list_of_reviews:
+#     print(i.rating, i.text)
 #
-# print(first_review.rating, first_review.text)
-# # first_review = BusinessReview()
+# for raw_business_data['business_name']:
+#     print(raw_business_data['business_name'])
 
-
-
-#-------------------------------------------------------
-# for item in raw_business_data[0]['reviews'][:]:
-#     for key, value in item.items():
-#         if key == 'rating':
-#             salt_review.rating.append(value)
-#         else:
-#             salt_review.review_text.append(value)
+# voodoo = Business('Voodoo', [])
+#
+# new_list = [Review(3, 'great!')]
 #
 #
-# for item in raw_business_data[1]['reviews'][:]:
-#     for key, value in item.items():
-#         if key == 'rating':
-#             voodoo_review.rating.append(value)
-#         else:
-#             voodoo_review.review_text.append(value)
-#-------------------------------------------------------
+# voodoo = Business('Voodoo', new_list)
+#
+# print(voodoo)
+
+
+
+
+#
+# for x, y in word_pair_list:
+#     if x not in paired_word_dict:
+#         paired_word_dict[x] = {}
+#     if y not in paired_word_dict[x]:
+#         paired_word_dict[x][y] = 0
+#     paired_word_dict[x][y] += 1
+
+
+
+
+# new_dict = {k=business_name: {k2=rating: v=?, k3=text: v=?}}
