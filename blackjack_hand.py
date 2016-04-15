7f834a51376b77fcac823c565cb17f86d6a5ec14
Andrew <andrewtheis4$gmail.com>
2016-04-15 10:14:36 -0700

    nth commit: introduction to class functionality

--- blackjack_hand.py
+++ blackjack_hand.py
@@ -11,6 +11,9 @@ class Card:
             self.suit, self.value
         )
 
+    def __eq__(self, other):
+        pass
+
 class Hand:
     """docstring for Hand"""
     def __init__(self, card_list):
@@ -21,8 +24,8 @@ class Hand:
             self.card_list
         )
 
-    def hit(self):
-        pass
+    def add_card(self, new_card):
+        self.card_list.append(instantiate_card_add_to_list(new_card))
 
     def score_hand(self):
         score = 0
@@ -43,37 +46,56 @@ class Hand:
     def bust(self, score):
         pass
 
+
+class Deck:
+    """docstring for Deck"""
+    def __init__(self, list_of_cards):
+        self.list_of_cards = list_of_cards
+
+    def __repr__(self):
+        return 'Deck(card: {})'.format(
+            self.list_of_cards
+        )
+
 def prompt_user_for_cards():
-    raw_list_of_cards = []
+    raw_cards = []
 
     more_cards = True
     while more_cards:
         user_cards = input('Enter a suit and card value separated by a single space:\n(type \'done\' to continue)\n> ')
         if user_cards != 'done':
-            raw_list_of_cards.append(user_cards)
+            raw_cards.append(user_cards)
         else:
             more_cards = False
-    return raw_list_of_cards
+    return raw_cards
 
-def instantiate_card_objects_in_list(raw_list_of_cards):
-    split_suit_value_pair_list = []
+def instantiate_card_add_to_list(raw_list_of_cards):
     list_of_cards = []
 
     for item in raw_list_of_cards:
-        split_suit_value_pair_list.append(item.split())
-    for x, y in split_suit_value_pair_list:
-        list_of_cards.append(Card(x, y))
+        list_of_cards.append(Card((item.split())[0], (item.split())[1]))
     return list_of_cards
 
 def instantiate_hand(list_of_cards):
     return Hand(list_of_cards)
 
+def get_new_card():
+    return input('Enter a card suit and value separated by a single space:\n> ')
+
+
 def main():
-    raw_list_of_cards = prompt_user_for_cards()
-    list_of_cards = instantiate_card_objects_in_list(raw_list_of_cards)
+    # raw_list_of_cards = prompt_user_for_cards()
+    raw_list_of_cards = ['diamonds ace', 'pizza 4']
+
+    list_of_cards = instantiate_card_add_to_list(raw_list_of_cards)
     hand = instantiate_hand(list_of_cards)
     score = hand.score_hand()
 
+    print(raw_list_of_cards)
+    print(list_of_cards)
+
+    hand.add_card(get_new_card())
+    print(hand)
     print(score)
 
     if score > 21:
