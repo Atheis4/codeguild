ccc925f645773f81896b1a262f2b0d4b9a86ce41
Andrew <andrewtheis4$gmail.com>
2016-04-20 11:27:14 -0700

    added matplotlib graphing capabilities to yearly, monthly, and daily rain averages--still need to create a navigation system and the other sources of rain data.

--- rain.py
+++ rain.py
@@ -1,5 +1,7 @@
 import urllib.request
 import statistics as stats
+from matplotlib import pyplot as plt
+from pylab import axis
 
 def import_web_data():
     with urllib.request.urlopen('http://or.water.usgs.gov/non-usgs/bes/sunnyside.rain') as website:
@@ -7,12 +9,12 @@ def import_web_data():
     return lines
 
 def strip_split_slice_web_data(lines):
-    new_line_list = [line.strip()[:17].split() for line in lines[11:]]
-    return new_line_list
+    line_list = [line.strip()[:17].split() for line in lines[11:]]
+    return line_list
 
-def get_rid_of_null_data_turn_numbers_to_int(new_line_list):
+def normalize_data_convert_to_tuple(line_list):
     """raw y data in hundreths of inches. Conversion happens in print functions"""
-    x, y = list(zip(*new_line_list))
+    x, y = list(zip(*line_list))
     y = ' '.join(y).replace('-', '0').split()
     rainfall_int_list = []
     for item in y:
@@ -65,10 +67,106 @@ def print_date_with_most_rain_on_average(day_to_rainfall_total_dict):
         if v == most_rainfall:
             print('The day with the most rain on average is {0} with\n{1:.2f} inches of rain.'.format(k, v))
 
+def create_chrono_rain_lists(day_to_rainfall_total_dict):
+    list_rain = []
+
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'JAN' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'FEB' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'MAR' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'APR' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'MAY' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'JUN' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'JUL' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'AUG' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'SEP' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'OCT' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'NOV' in x:
+            list_rain.append(stats.mean(y))
+    for x, y in sorted(day_to_rainfall_total_dict.items()):
+        if 'DEC' in x:
+            list_rain.append(stats.mean(y))
+
+    return list_rain
+
+def month_to_rain_maker(rainfall_dict):
+    month_to_rain = {}
+
+    for date in rainfall_dict.keys():
+        month_to_rain[date[3:-5]] = []
+
+    for x, y in rainfall_dict.items():
+        month_to_rain[x[3:-5]].append(y)
+
+    return month_to_rain
+
+def graph_daily(rain_list):
+    date_range = []
+    for i in range(366):
+        date_range.append(i)
+
+    axis()
+    plt.plot(date_range, rain_list, marker="o")
+    plt.xlabel('date 0-365')
+    plt.ylabel('Rainfall in inches')
+    plt.title('Average rainfall per day')
+    plt.show()
+
+def graph_monthly(month_to_rain):
+    y_list = []
+    x_list = []
+
+    for i in range(1, 13):
+        x_list.append(i)
+
+    for y in month_to_rain.values():
+        y_list.append(stats.mean(y))
+
+    axis(xmin=0, xmax=14, ymin=0, ymax=0.25)
+    plt.plot(x_list, y_list, marker='o')
+    plt.xlabel('Month')
+    plt.ylabel('Rainfall in inches')
+    plt.title('Month to rainfall average')
+    plt.show()
+
+def graph_yearly(year_to_rainfall_total_dict):
+    x_list = []
+    y_list = []
+
+    for x, y in sorted(year_to_rainfall_total_dict.items()):
+        x_list.append(x)
+        y_list.append(y)
+
+    axis(xmin=2000, xmax=2017, ymin=0, ymax=70)
+    plt.plot(x_list, y_list, marker='o')
+    plt.xlabel('x = Year')
+    plt.ylabel('y = average rainfall in inches')
+    plt.title('Average yearly rainfall at Sunnyside Elementary, Portland, OR')
+    plt.show()
+
 def main():
-    lines = import_web_data()
-    new_line_list = strip_split_slice_web_data(lines)
-    date_to_rain_tuple = get_rid_of_null_data_turn_numbers_to_int(new_line_list)
+    line_list = strip_split_slice_web_data(import_web_data())
+    date_to_rain_tuple = normalize_data_convert_to_tuple(line_list)
     rainfall_dict = create_dict_from_data_tuple(date_to_rain_tuple)
     print()
     print_single_day_with_most_rain(rainfall_dict)
@@ -78,15 +176,34 @@ def main():
     day_to_rainfall_total_dict = create_avg_day_to_rain_total_dict(rainfall_dict)
     print()
     print_date_with_most_rain_on_average(day_to_rainfall_total_dict)
+    month_to_rain = month_to_rain_maker(rainfall_dict)
 
-    user_choice = True
+    rain_list = create_chrono_rain_lists(day_to_rainfall_total_dict)
 
-    while user_choice:
-        print()
-        date_lookup = input('Would you like to know the future\'s weather?\n(Enter date as DD-MMM, ex. 04-MAR -- type \'done\' to quit\n> ')
-        if date_lookup in day_to_rainfall_total_dict.keys():
-            print('If past is prologue: On {0} it might rain {1:.2f} inches.\nOr not.'.format(date_lookup, stats.mean(day_to_rainfall_total_dict[date_lookup])))
-        if date_lookup == 'done':
-            user_choice = False
+    # graph_yearly(year_to_rainfall_total_dict)
+    # graph_daily(rain_list)
+    # graph_monthly(month_to_rain)
 
-main()
+
+
+
+
+
+
+
+
+
+
+    # user_choice = True
+
+    # while user_choice:
+    #     print()
+    #     date_lookup = input('Would you like to know the future\'s weather?\n(Enter date as DD-MMM, ex. 04-MAR -- type \'done\' to quit\n> ')
+    #     if date_lookup in day_to_rainfall_total_dict.keys():
+    #         print('If past is prologue: On {0} it might rain {1:.2f} inches.\nOr not.'.format(date_lookup, stats.mean(day_to_rainfall_total_dict[date_lookup])))
+    #     if date_lookup == 'done':
+    #         user_choice = False
+
+
+if __name__ == "__main__":
+    main()
