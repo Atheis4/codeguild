47b1aa5b52417ee6792e6fc87006e71ae15028a8
Andrew <andrewtheis4$gmail.com>
2016-04-20 11:28:24 -0700

    test file for tic_tac.py

--- tic_tac_test.py
+++ tic_tac_test.py
@@ -1,7 +1,10 @@
 import tic_tac
 import unittest
 
+
 class Board(unittest.TestCase):
+    # List Board Testing
+
     def test_place_list(self):
         board1 = tic_tac.ListListTTTBoard()
         board1.place(1, 2, 'X')
@@ -28,6 +31,13 @@ class Board(unittest.TestCase):
         board1.place(0, 2, 'O')
         assert board1.won() == 'O'
 
+    def test_print_board_list(self):
+        board1 = tic_tac.ListListTTTBoard()
+        board1.place(1, 1, 'X')
+        assert board1.__str__() == ' | | \n |X| \n | | \n'
+
+    # Dict Board Testing
+
     def test_place_dict(self):
         board2 = tic_tac.DictTTTBoard()
         board2.place(1, 1, 'X')
@@ -53,3 +63,16 @@ class Board(unittest.TestCase):
         board2.place(1, 0, 'X')
         board2.place(2, 2, 'X')
         assert board2.won() == None
+
+    def test_print_board_dict(self):
+        board1 = tic_tac.DictTTTBoard()
+        board1.place(1, 1, 'X')
+        assert board1.__str__() == ' | | \n |X| \n | | \n'
+
+    # Coord Board Testing
+
+    def test_place_coord(self):
+        board3 = tic_tac.CoordsTTTBoard()
+        board3.place(1, 1, 'X')
+        board3.place(1, 0, 'X')
+        assert board3.x_y_token_triplets == [(1, 1, 'X'), (1, 0, 'X')]
