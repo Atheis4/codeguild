8924cb1d2dce6d22cd41d6553d1e7403353a6047
Andrew <andrewtheis4$gmail.com>
2016-05-05 22:13:51 -0700

    styleguide changes

--- album2.js
+++ album2.js
@@ -1,63 +1,66 @@
-"use strict";
+'use strict';
 
-function getUrl () {
-  return $("#url-input").val();
+function getUrl() {
+  return $('#url-input').val();
 }
 
-function createImgElem () {
-  var imgElement = $("<img>").attr("src", getUrl());
+function createImgElem() {
+  var imgElement = $('<img>').attr('src', getUrl());
   return imgElement;
 }
 
-function createImgLink () {
-  var imgLink = $("<a></a>").text("Image Source").attr("href", getUrl());
+function createImgLink() {
+  var imgLink = $('<a></a>').text('Image Source').attr('href', getUrl());
   return imgLink;
 }
 
-function createTile () {
+function createTile() {
   var imgElement = createImgElem();
   var imgLink = createImgLink();
-  var newTile = $("<div></div>").append(imgElement, imgLink);
+  var newTile = $('<div></div>').append(imgElement, imgLink);
   return newTile;
 }
 
-function createDelLink (newTile) {
-  var delLink = $("<button></button>").text("remove").attr("href", "");
-  delLink.on("click", function (event) {
+function createDelLink(newTile) {
+  var delLink = $('<button></button>').text('remove').attr('href', '');
+  delLink.on('click', function(event) {
     event.preventDefault();
-    newTile.remove("div");
-    $("h2").text("Current Tile(s): " + countTiles());
+    newTile.remove('div');
+    $('h2').text('Current Tile(s): ' + countTiles());
   });
   return newTile.append(delLink);
 }
 
-function addTileToGallery (newTile) {
-  $("#gallery").append(newTile)
+function addTileToGallery(newTile) {
+  $('#gallery').append(newTile);
 }
 
-function createTileAndAddToGallery () {
+function createTileAndAddToGallery() {
   var newTile = createDelLink(createTile());
   addTileToGallery(newTile);
 }
 
-function countTiles () {
-  var tileCount = $("div").length;
+function countTiles() {
+  var tileCount = $('div').length;
   return tileCount;
 }
 
-function clearTextBox () {
-  $("textbox").text();
+function clearTextBox() {
+  $('textbox').text();
 }
 
-function registerEventHandlers () {
-  $("form").on("submit", function (event) {
-    event.preventDefault ();
-    createTileAndAddToGallery ();
-    $("h2").text("Current Tile(s): " + countTiles());
-    $("#url-input").val("");
-  })
+
+
+
+
+
+function registerEventHandlers() {
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    createTileAndAddToGallery();
+    $('h2').text('Current Tile(s): ' + countTiles());
+    $('#url-input').val('');
+  });
 }
 
-$(document).ready(function () {
-  registerEventHandlers();
-});
+$(document).ready(registerEventHandlers);
