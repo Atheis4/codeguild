2d48a0e891ea22ea42bbd0af9e6f64dfa4d4d8cb
Andrew <andrewtheis4$gmail.com>
2016-05-16 23:08:01 -0700

    committing reorganization of codeguild folder

--- blackjack_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import blackjack
-
-class Blackjack(unittest.TestCase):
-    def test_deck_init(self):
-        cards_list = ['ace', 'spades']
-        deck = blackjack.Deck(cards_list)
-        assert deck.cards == ['ace', 'spades']
-
