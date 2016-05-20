4cf91b50aaebbb1bad04cd2d40a6a6e2e702f021
Andrew <andrewtheis4$gmail.com>
2016-05-20 09:54:58 -0700

    second commit: slight .css/.html improvements... cannot--for the life of me--get the time stamp to display the local time. Stuck in UTC...

--- flutter/flutter/models.py
+++ flutter/flutter/models.py
@@ -5,17 +5,19 @@ class Flutt(models.Model):
     """docstring for Flutt"""
     user_name = models.TextField(null=True)
     text = models.TextField()
-    date_created = models.DateTimeField(editable=False)
+    date_created = models.DateTimeField(
+        default=timezone.now,
+    )
 
     def __str__(self):
         return '{!r}: {!r}, {!r}'.format(
             self.user_name,
             self.text,
-            self.date_created
+            self.date_created,
         )
 
     def __repr__(self):
-        return 'Flutt(User:{!r}, Text:{!r}, Created Date: {!r})'.format(
+        return 'Flutt(user_name:{!r}, text:{!r}, date_created: {!r})'.format(
             self.user_name,
             self.text,
             self.date_created,
